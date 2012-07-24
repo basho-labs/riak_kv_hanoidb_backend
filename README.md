9f17a1501d460a938ee4199e2e51054bc4889ae4
Gregory Burd <greg@basho.com>
2012-07-24 18:00:55 -0400

    Import from HanoiDB repository, split Riak-specific pieces into this repo.

---
+++ README.md
@@ -0,0 +1,66 @@
+# The (Towers of) HanoiDB Backend for Riak/KV
+
+This storage engine can function as an alternative backend for Basho's Riak/KV.
+See also: [HanoiDB](https://github.com/basho/hanoidb)
+
+### Configuration options
+Put these values in your `app.config` in the `hanoidb` section
+
+```erlang
+ {hanoidb, [
+          {data_root, "./data/hanoidb"},
+
+          %% Enable/disable on-disk compression.
+          %%
+          {compress, none | gzip | snappy | lz4},
+
+          %% Expire (automatically delete) entries after N seconds.
+          %% When this value is 0 (zero), entries never expire.
+          %%
+          {expiry_secs, 0},
+
+          %% Sync strategy `none' only syncs every time the
+          %% nursery runs full, which is currently hard coded
+          %% to be evert 256 inserts or deletes.
+          %%
+          %% Sync strategy `sync' will sync the nursery log
+          %% for every insert or delete operation.
+          %%
+          {sync_strategy, none | sync | {seconds, N}},
+
+          %% The page size is a minimum page size, when a page fills
+          %% up to beyond this size, it is written to disk.
+          %% Compression applies to such units of page size.
+          %%
+          {page_size, 8192},
+
+          %% Read/write buffer sizes apply to merge processes.
+          %% A merge process has two read buffers and a write
+          %% buffer, and there is a merge process *per level* in
+          %% the database.
+          %%
+          {write_buffer_size, 524288},  % 512kB
+          {read_buffer_size, 524288},  % 512kB
+
+          %% The merge strategy is one of `fast' or `predictable'.
+          %% Both have same log2(N) worst case, but `fast' is
+          %% sometimes faster; yielding latency fluctuations.
+          %%
+          {merge_strategy, fast | predictable}
+         ]},
+```
+
+### How to deploy HanoiDB as a Riak/KV backend
+
+Deploy `hanoidb` into a Riak devrel cluster using the `enable-hanoidb`
+script. Clone the `riak` repo, change your working directory to it, and then
+execute the `enable-hanoidb` script. It adds `hanoidb` as a dependency, runs
+`make all devrel`, and then modifies the configuration settings of the
+resulting dev nodes to use the hanoidb storage backend.
+
+1. `git clone git://github.com/basho/riak.git`
+1. `mkdir riak/deps`
+1. `cd riak/deps`
+1. `git clone git://github.com/basho/riak_kv_hanoidb_backend.git`
+1. `cd ..`
+1. `./deps/riak_kv_hanoidb_backend/enable-hanoidb`
---
+++ test/hanoidb_drv.erl
@@ -0,0 +1,143 @@
+%% ----------------------------------------------------------------------------
+%%
+%% hanoidb: LSM-trees (Log-Structured Merge Trees) Indexed Storage
+%%
+%% Copyright 2011-2012 (c) Trifork A/S.  All Rights Reserved.
+%% http://trifork.com/ info@trifork.com
+%%
+%% Copyright 2012 (c) Basho Technologies, Inc.  All Rights Reserved.
+%% http://basho.com/ info@basho.com
+%%
+%% This file is provided to you under the Apache License, Version 2.0 (the
+%% "License"); you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+%% License for the specific language governing permissions and limitations
+%% under the License.
+%%
+%% ----------------------------------------------------------------------------
+
+%% @doc Drive a set of LSM BTrees
+-module(hanoidb_drv).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+-export([
+         delete_exist/2,
+         get_exist/2,
+         get_fail/2,
+         open/1, close/1,
+         put/3,
+         fold_range/4,
+         stop/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, { btrees = dict:new() % Map from a name to its tree
+               }).
+
+%%%===================================================================
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+call(X) ->
+    gen_server:call(?SERVER, X, infinity).
+
+get_exist(N, K) ->
+    call({get, N, K}).
+
+get_fail(N, K) ->
+    call({get, N, K}).
+
+delete_exist(N, K) ->
+    call({delete_exist, N, K}).
+
+open(N) ->
+    call({open, N}).
+
+close(N) ->
+    call({close, N}).
+
+put(N, K, V) ->
+    call({put, N, K, V}).
+
+fold_range(T, Fun, Acc0, Range) ->
+    call({fold_range, T, Fun, Acc0, Range}).
+
+stop() ->
+    call(stop).
+
+%%%===================================================================
+
+init([]) ->
+    {ok, #state{}}.
+
+handle_call({open, N}, _, #state { btrees = D} = State) ->
+    case hanoidb:open(N) of
+        {ok, Tree} ->
+            {reply, ok, State#state { btrees = dict:store(N, Tree, D)}};
+        Otherwise ->
+            {reply, {error, Otherwise}, State}
+    end;
+handle_call({close, N}, _, #state { btrees = D} = State) ->
+    Tree = dict:fetch(N, D),
+    case hanoidb:close(Tree) of
+        ok ->
+            {reply, ok, State#state { btrees = dict:erase(N, D)}};
+        Otherwise ->
+            {reply, {error, Otherwise}, State}
+    end;
+handle_call({fold_range, Name, Fun, Acc0, Range},
+            _From,
+            #state { btrees = D } = State) ->
+    Tree = dict:fetch(Name, D),
+    Result = hanoidb:fold_range(Tree, Fun, Acc0, Range),
+    {reply, Result, State};
+handle_call({put, N, K, V}, _, #state { btrees = D} = State) ->
+    Tree = dict:fetch(N, D),
+    case hanoidb:put(Tree, K, V) of
+        ok ->
+            {reply, ok, State};
+        Other ->
+            {reply, {error, Other}, State}
+    end;
+handle_call({delete_exist, N, K}, _, #state { btrees = D} = State) ->
+    Tree = dict:fetch(N, D),
+    Reply = hanoidb:delete(Tree, K),
+    {reply, Reply, State};
+handle_call({get, N, K}, _, #state { btrees = D} = State) ->
+    Tree = dict:fetch(N, D),
+    Reply = hanoidb:get(Tree, K),
+    {reply, Reply, State};
+handle_call(stop, _, #state{ btrees = D } = State ) ->
+    [ hanoidb:close(Tree) || {_,Tree} <- dict:to_list(D) ],
+    {stop, normal, ok, State};
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
